66a7d980078429c9ecf0867eaaf4207edb6ca5a6
ndunas <ndunas@student.wsb-nlu.edu.pl>
2025-04-24 16:37:55 +0200

    [BSH-18][17.1] Define constants for grid rendering
     -Added VisualConfigs.h for storing constants for visuals
     -modified CMakeLists.txt in render folder to read all .h and.cpp files in folder

---
+++ source/render/include/VisualConfigs.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace VisualConfig
+{
+	inline constexpr int CELL_SIZE = 40;
+	inline constexpr int GRID_OFFSET_X = 100;
+	inline constexpr int GRID_OFFSET_Y = 100;
+	inline constexpr int GRID_LINE_THICKNESS = 2;
+	inline constexpr int GRID_LINE_COLOR = 0xAAAAAAFF; // gray
+}
