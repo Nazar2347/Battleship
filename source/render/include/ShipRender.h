85985fa34dc3da8237cb0988a6e274d9c347fc75
ndunas <ndunas@student.wsb-nlu.edu.pl>
2025-04-25 14:00:01 +0200

    [BSH-21][17.4.2] Add rendering ghost ships  - Added inline method of getting Ship color by it's type  - Added new method of drawGhostShip

--- source/render/include/ShipRender.h
+++ source/render/include/ShipRender.h
@@ -2,11 +2,27 @@
 #include "Ship.h"
 #include <raylib.h>
 
-
+inline Color getShipTypeColor(ShipType shipType)
+{
+	switch (shipType)
+	{
+	case ShipType::FRIGATE:
+		return DARKGRAY;
+	case ShipType::DESTROYER:
+		return DARKBLUE;
+	case ShipType::CRUISER:
+		return RED;
+	case ShipType::LINKOR:
+		return DARKPURPLE;
+	default:
+		return DARKGRAY;
+	}
+}
 
 
 class ShipRender
 {
 public:
     void DrawShips(const std::vector<Ship>& ships);
+    void drawGhostShip(const Ship& ship, Color color);
 };
