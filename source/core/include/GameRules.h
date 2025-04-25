c627f78a5f81fc12a3ffb460519b569f932e2440
ndunas <ndunas@student.wsb-nlu.edu.pl>
2025-04-25 13:16:58 +0200

    [BSH-43] Refactor AIPlayer::placeShips  Added constant vector of ShipTypes in GameRules.h

--- source/core/include/GameRules.h
+++ source/core/include/GameRules.h
@@ -6,11 +6,11 @@ namespace GameRules
 {
 	constexpr std::size_t BOARD_SIZE = 10;
 
-    inline const std::unordered_map<ShipType, int> SHIP_COUNTS = {
-        {ShipType::FRIGATE,4},
-        {ShipType::DESTROYER,3},
-        {ShipType::CRUISER, 2},
-        {ShipType::LINKOR, 1}
-    };
-
-}
+	inline const std::vector<ShipType> shipsToPlace = {
+		ShipType::LINKOR,
+		ShipType::CRUISER, ShipType::CRUISER,
+		ShipType::DESTROYER, ShipType::DESTROYER, ShipType::DESTROYER,
+		ShipType::FRIGATE, ShipType::FRIGATE, ShipType::FRIGATE, ShipType::FRIGATE
+	};
+	
+};
--- source/core/src/AIPlayer.cpp
+++ source/core/src/AIPlayer.cpp
@@ -27,73 +27,24 @@ Orientation AIPlayer::horizontalOrVertical()
 void AIPlayer::placeShips()
 {
 	m_ships.clear();
-	/*required to refactor this method to make it more efficient template for creating different types of ships for the board*/
-		//Creating ships and placing them on board
 	using namespace GameRules;
-	for (int frigates = 4; frigates > 0; frigates--)
+
+	for (auto ship: GameRules::shipsToPlace)
 	{
 		bool isPlaced = false;
 		while (!isPlaced)
 		{
 
-			Ship frigate(ShipType::FRIGATE, generateRandomCoordinate(), horizontalOrVertical());			
-			if (m_board.isValidPlacement(frigate))
+			Ship AIShip(ship, generateRandomCoordinate(), horizontalOrVertical());			
+			if (m_board.isValidPlacement(AIShip))
 			{
-				m_ships.push_back(frigate);
-				m_board.placeShip(frigate);
+				m_ships.push_back(AIShip);
+				m_board.placeShip(AIShip);
 				isPlaced = true;
 			}
 		}
 
 	}
-	for (int destroyer = 3; destroyer > 0;destroyer--)
-	{
-		bool isPlaced = false;
-		while (!isPlaced)
-		{
-
-			Ship frigate(ShipType::DESTROYER, generateRandomCoordinate(), horizontalOrVertical());
-			if (m_board.isValidPlacement(frigate))
-			{
-				m_ships.push_back(frigate);
-				m_board.placeShip(frigate);
-				isPlaced = true;
-			}
-		}
-	}
-	for (int cruisers = 2; cruisers > 0;cruisers--)
-	{
-		bool isPlaced = false;
-		while (!isPlaced)
-		{
-
-			Ship cruiser(ShipType::CRUISER, generateRandomCoordinate(), horizontalOrVertical());
-			if (m_board.isValidPlacement(cruiser))
-			{
-				m_ships.push_back(cruiser);
-				m_board.placeShip(cruiser);
-				isPlaced = true;
-			}
-		}
-	}
-	for (int linkors = 1; linkors > 0; linkors--)
-	{
-		bool isPlaced = false;
-		while (!isPlaced)
-		{
-
-			Ship linkor(ShipType::LINKOR, generateRandomCoordinate(), horizontalOrVertical());
-			if (m_board.isValidPlacement(linkor))
-			{
-				m_ships.push_back(linkor);
-				m_board.placeShip(linkor);
-				isPlaced = true;
-			}
-		}
-	}
-
-
-
 }
 
 Coordinate AIPlayer::getNextMove()
