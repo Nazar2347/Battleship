8bf1c9d14a13da92eb6a590c8ae8ffe49b3402d0
ndunas <ndunas@student.wsb-nlu.edu.pl>
2025-04-26 10:59:57 +0200

    [BSH-18] [17.1.3] Define constans for grid rendering
    - Added two new constants for location of ENEMY GRID (BOARD)

--- source/render/include/VisualConfigs.h
+++ source/render/include/VisualConfigs.h
@@ -8,4 +8,6 @@ namespace VisualConfig
 	inline constexpr int GRID_LINE_THICKNESS = 2;
 	inline constexpr int GRID_LINE_COLOR = 0xAAAAAAFF; // gray
 	inline constexpr int ENEMY_GRID_GAP = 500; //100px gap between boards
+	inline constexpr int ENEMY_GRID_OFFSET_X = ENEMY_GRID_GAP + GRID_OFFSET_X;
+	inline constexpr int ENEMY_GRID_OFFSET_Y = GRID_OFFSET_Y;
 }
