c8aca23bde0b623d0df453df89a7fc87b4567763
ndunas <ndunas@student.wsb-nlu.edu.pl>
2025-04-26 13:17:32 +0200

    [BSH-17.5] Draw Hits and misses on board  - Added two new functions to GridRender     -displayPlayerCellState()     -displayEnemyCellState()     wich both just go through  all cells on the board and     draw His and Misses

--- source/render/src/GridRender.cpp
+++ source/render/src/GridRender.cpp
@@ -57,3 +57,54 @@ void GridRender::drawEnemyGrid()
 		);
 	}
 }
+
+void GridRender::displayPlayerCellStates(Board board)
+{
+
+	using namespace GameRules;
+	using namespace VisualConfig;
+	Vector2 board_origin = { GRID_OFFSET_X, GRID_OFFSET_Y };
+	for (int y = 0; y < BOARD_SIZE; y++)
+	{
+		for (int x = 0; x < BOARD_SIZE; x++)
+		{
+			if (board.getCellState({ x,y }) == CellState::EMPTY)
+			{
+				// empty
+			}
+			else if (board.getCellState({ x,y }) == CellState::HIT)
+			{
+				DrawHitMark({ x,y }, board_origin);
+			}
+			else if (board.getCellState({ x,y }) == CellState::MISS)
+			{
+				DrawMissMark({ x,y }, board_origin);
+			}
+		}
+	}
+}
+void GridRender::displayEnemyCellStates(Board board)
+{
+	using namespace GameRules;
+	using namespace VisualConfig;
+	Vector2 board_origin = { ENEMY_GRID_OFFSET_X, ENEMY_GRID_OFFSET_Y };
+	for (int y = 0; y < BOARD_SIZE; y++)
+	{
+		for (int x = 0; x < BOARD_SIZE; x++)
+		{
+			if (board.getCellState({ x,y }) == CellState::EMPTY)
+			{
+				// empty
+			}
+			else if (board.getCellState({ x,y }) == CellState::HIT)
+			{
+				DrawHitMark({ x,y }, board_origin);
+			}
+			else if (board.getCellState({ x,y }) == CellState::MISS)
+			{
+				DrawMissMark({ x,y }, board_origin);
+			}
+		}
+	}
+
+}
