60796840e2716577933e92c697ada3ed030fcc18
ndunas <ndunas@student.wsb-nlu.edu.pl>
2025-04-28 18:32:18 +0200

    [BSH-68][65.3] Implement Unit test for Ship class -removed Ship::isSunked test from test.cpp - Added new .cpp file for Ship unit tests - Added Test_case for checking of Ship::ISSunk method with 3 sections

---
+++ tests/test_Ship.cpp
@@ -0,0 +1,31 @@
+#include <catch_test_macros.hpp>
+
+#include <cstdint>
+#include "Ship.h"
+
+
+TEST_CASE("Ship::IsSunk() works correctly ", "[Ship]")
+{
+
+    Ship ship(ShipType::CRUISER, { 0,0 }, Orientation::HORIZONTAL);
+
+
+    SECTION("Ship is not sunk initially")
+    {
+        REQUIRE(ship.ISSunk() == false);
+    }
+
+    SECTION("Ship is sunk after all parts are hit")
+    {
+        ship.registerHit({ 0, 0 });
+        ship.registerHit({ 1, 0 });
+        ship.registerHit({ 2, 0 });
+        REQUIRE(ship.ISSunk() == true);
+    }
+
+    SECTION("Ship is not sunk if only some parts are hit")
+    {
+        ship.registerHit({ 0, 0 });
+        REQUIRE(ship.ISSunk() == false);
+    }
+}
