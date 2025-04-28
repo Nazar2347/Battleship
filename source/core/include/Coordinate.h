4528495cd06b4d1da23e5dfcb3204bc95ea2bc6b
ndunas <ndunas@student.wsb-nlu.edu.pl>
2025-04-28 18:26:39 +0200

    [BSH-65][65.1] Add Hash function for Coordinate structure

--- source/core/include/Coordinate.h
+++ source/core/include/Coordinate.h
@@ -12,3 +12,13 @@ struct Coordinate
 		return x == other.x && y == other.y;
 	}
 };
+//Hash function for coordinates for unordored_set
+namespace std
+{
+	template<>
+	struct hash<Coordinate> {
+		std::size_t operator()(const Coordinate& coord) const {
+			return std::hash<int>()(coord.x) ^ (std::hash<int>()(coord.y) << 1);
+		}
+	};
+}
